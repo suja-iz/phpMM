de2a70640550cc81d17b61c538754ab0dd93b1b2
suja-iz <90198636+suja-iz@users.noreply.github.com>
2021-09-08 09:51:53 +0500

    styling
    CSS updated on index.html and practice files

--- practice/ch02/scripts/getEmpInfo.php
+++ practice/ch02/scripts/getEmpInfo.php
@@ -9,13 +9,25 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title></title>
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+	<title>EmpInforDisplay</title>
 </head>
 <body>
+<nav class="navbar navbar-light bg-light">
+		<div class="container-fluid">
+		  <span class="navbar-brand mb-0 h1">Employee.inc</span>
+		</div>
+	  </nav>
+	  <div class="container">
 	<h1>Registered Employee</h1>
 <p>Employee ID: <?php echo $employee_id ?></p>
 <p>Name : <?php echo $employee_name ?></p>
 <p>Occupation : <?php echo $ocupatoion ?></p>
 <p>Phone: <?php echo $phone ?></p>
+<a href="../employee.html"  class="btn btn-primary">Register New Employee</a>
+</div>
+
 </body>
 </html>
